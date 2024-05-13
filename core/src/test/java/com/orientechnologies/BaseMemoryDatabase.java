fac9a703e3e2c7797cd50af617c9a1255cf9e485
laa <andrii0lomakin@gmail.com>
2024-05-13 12:58:09 +0200

    Assertion timout was increased for tests with timings.

--- core/src/test/java/com/orientechnologies/BaseMemoryDatabase.java
+++ core/src/test/java/com/orientechnologies/BaseMemoryDatabase.java
@@ -28,13 +28,14 @@ public class BaseMemoryDatabase {
     db = context.open(this.databaseName, "admin", "adminpwd");
   }
 
+  @SuppressWarnings("SameParameterValue")
   protected void reOpen(String user, String password) {
     this.db.close();
     this.db = context.open(this.databaseName, user, password);
   }
 
   @After
-  public void afterTest() throws Exception {
+  public void afterTest() {
     db.close();
     context.drop(databaseName);
     context.close();
@@ -42,7 +43,7 @@ public class BaseMemoryDatabase {
 
   public static void assertWithTimeout(ODatabaseDocument session, Runnable runnable)
       throws Exception {
-    for (int i = 0; i < 10 * 60 * 10; i++) {
+    for (int i = 0; i < 30 * 60 * 10; i++) {
       try {
         session.begin();
         runnable.run();
